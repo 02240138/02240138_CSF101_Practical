d95f683d3aea1068b12819f6994228aaae817f10
ugyen dema <demau49@gmail.com>
2025-03-06 00:09:01 +0600

    my first commit

---
+++ practical0.py
@@ -0,0 +1,3 @@
+studentName = "Ugyen Dema"
+studentNumber = "02240138"
+print ("My name is "+studentName+" and my student number is", studentNumber)
---
+++ practical1.py
@@ -0,0 +1,24 @@
+#color = "indigo"
+#age = 19
+#math_marks = 85.50
+#sleep = True 
+
+#print(color)
+#print(age)
+#print(math_marks)
+#print(sleep)
+
+
+
+#x=5
+#y=4
+#print(x*y)
+
+placeA_temp = 20
+placeb_temp= 30
+if placeA_temp > placeb_temp:
+    print("Place A is hotter than Place B")
+elif placeA_temp < placeb_temp:
+    print("Placd b is hotter than place A")
+else:
+    print("both places have same temperature")    
